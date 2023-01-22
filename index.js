7a1312806a451170c34dcfbd9ae93b210fdd1c52
Hubert Gorzkowski <h-gorzkowski1@wp.pl>
2023-01-22 14:24:31 +0100

    Rearange source code to seperate functions

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ document.addEventListener("DOMContentLoaded", function () {
       isThrottled = false;
     }, 1000);
     const direction = e.wheelDelta < 0 ? 1 : -1;
+    scroll(direction);
+  });
+
+  function scroll(direction) {
     if (direction === 1) {
       const isLastSection = currentSectionIndex === sections.length - 1;
       if (isLastSection) return;
@@ -21,9 +25,13 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     currentSectionIndex = currentSectionIndex + direction;
 
+    scrollToCurrentSection();
+  }
+
+  function scrollToCurrentSection() {
     sections[currentSectionIndex].scrollIntoView({
       behavior: "smooth",
       block: "start",
     });
-  });
+  }
 });
