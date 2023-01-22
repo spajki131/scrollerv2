171f509402ba474752da637214f37f661806daf2
Hubert Gorzkowski <h-gorzkowski1@wp.pl>
2023-01-22 14:18:39 +0100

    Add scroll feature

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
   const rootElement = document.querySelector("#root");
   const sections = document.querySelectorAll("section");
+  let currentSectionIndex = 0;
+  let isThrottled = false;
 
   document.addEventListener("mousewheel", function (e) {
-    console.log(e.wheelDelta);
+    if (isThrottled) return;
+    isThrottled = true;
+
+    setTimeout(function () {
+      isThrottled = false;
+    }, 1000);
+    const direction = e.wheelDelta < 0 ? 1 : -1;
+    if (direction === 1) {
+      const isLastSection = currentSectionIndex === sections.length - 1;
+      if (isLastSection) return;
+    } else if (direction === -1) {
+      const isFirstSection = currentSectionIndex === 0;
+      if (isFirstSection) return;
+    }
+    currentSectionIndex = currentSectionIndex + direction;
+
+    sections[currentSectionIndex].scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
   });
 });
